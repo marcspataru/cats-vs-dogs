4c3a0c18aaa46e735be7dc9894b9b785510f008f
Marc Spataru <marc.spataru@gmail.com>
2021-10-26 21:06:34 +0100

    set up repo

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { InferGetServerSidePropsType, NextPage } from "next";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch("https://postman-echo.com/get?foo1=bar1&foo2=bar2");
+  const data = await res.json();
+  console.log("This is what I have");
+  console.log(data.args);
+  return { props: data };
+};
+
+const Home: NextPage = (
+  prop: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
+  return <p>Am primit {prop.args.foo1} de la nenea Postasu</p>;
+};
+
+export default Home;
