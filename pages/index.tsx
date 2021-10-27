00c91154b2cc3cf5f3f2a472518648df6df925dc
Marc Spataru <marc.spataru@gmail.com>
2021-10-27 21:31:15 +0100

    MVP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { InferGetServerSidePropsType, NextPage } from "next";
 import { GetServerSideProps } from "next";
+import Image from "next/image";
+import { useState } from "react";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const res = await fetch("https://postman-echo.com/get?foo1=bar1&foo2=bar2");
@@ -10,7 +12,49 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const Home: NextPage = (
   prop: InferGetServerSidePropsType<typeof getServerSideProps>
 ) => {
-  return <p>Am primit {prop.args.foo1} de la nenea Postasu</p>;
+  const [image, setImage] = useState<File>();
+  const [createObjectURL, setCreateObjectURL] = useState("/vercel.svg");
+  const [catOrDog, setCatOrDog] = useState("idk");
+
+  const uploadToClient = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+
+      setImage(i);
+      setCreateObjectURL(URL.createObjectURL(i));
+    }
+  };
+
+  const uploadToServer = async () => {
+    const body = new FormData();
+    if (image) body.append("image", image);
+    const response = await fetch("http://localhost:5000/api/predict", {
+      method: "POST",
+      body,
+    });
+    console.log(response.json().then((res) => setCatOrDog(res)));
+  };
+
+  return (
+    <div>
+      <Image
+        src={createObjectURL}
+        alt="Uploaded image"
+        width={300}
+        height={200}
+      />
+      <h4>Select Image</h4>
+      <input type="file" name="myImage" onChange={uploadToClient} />
+      <button
+        className="btn btn-primary"
+        type="submit"
+        onClick={uploadToServer}
+      >
+        Send to server
+      </button>
+      <p>I believe this is a {catOrDog}.</p>
+    </div>
+  );
 };
 
 export default Home;
